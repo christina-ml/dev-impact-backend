b28d7b79cb703f2783be344b96fb951104522f77
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-10 12:16:26 -0500

    set up dotenv with port name specified

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(9000, () => "App is running on port 9000");
+require('dotenv').config();
+
+app.listen(process.env.PORT, () => `App is running on port 7040`);
